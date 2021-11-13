4a565bf17b15d8506499f2b551e1d54435e5f287
Aleksei Pirogov <astynax@users.noreply.github.com>
2021-11-13 14:01:17 +0300

    add a lesson #8

---
+++ Lesson08.hs
@@ -0,0 +1,168 @@
+{-# LANGUAGE InstanceSigs #-}
+-- | Reader, Writer, State
+
+module Lesson08 where
+
+import Control.Monad (when)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Monoid (Sum(..))
+
+newtype Reader env a = Reader { runReader :: env -> a }
+
+ask :: Reader env env
+ask = Reader id
+
+asks :: (env -> a) -> Reader env a
+asks = Reader
+
+instance Functor (Reader env) where
+  fmap f x = Reader $ \env -> f $ runReader x env
+
+instance Applicative (Reader env) where
+  pure = Reader . const
+  af <*> ax = Reader $ \env ->
+    let
+      f = runReader af env
+      x = runReader ax env
+    in f x
+
+instance Monad (Reader env) where
+  (>>=) :: Reader env a
+        -> (a -> Reader env b)
+        -> Reader env b
+  mx >>= f = Reader $ \env ->
+    let x = runReader mx env
+    in runReader (f x) env
+
+data Config = Config
+  { port   :: Int
+  , host   :: String
+  , silent :: Bool
+  }
+
+app :: Config -> IO ()
+app = runReader $ do
+  p <- asks port
+  -- ...
+  -- ...
+  s <- asks silent
+  if (not s)
+    then do
+      a <- addr
+      pure $ putStrLn a
+    else
+      pure $ pure ()
+
+  where
+    addr = do
+      p <- asks port
+      h <- asks host
+      pure $ h <> ":" <> show p
+
+-- | Writer
+
+newtype Writer w a = Writer { runWriter :: (a, w) }
+
+tell :: w -> Writer w ()
+tell x = Writer ((), x)
+
+instance Functor (Writer w) where
+  fmap f x =
+    let (x', w) = runWriter x
+    in Writer (f x', w)
+
+instance Monoid w => Applicative (Writer w) where
+  pure :: a -> Writer w a
+  pure x = Writer (x, mempty)
+
+  wf <*> wx =
+    let
+      (f, w1) = runWriter wf
+      (x, w2) = runWriter wx
+    in Writer (f x, w1 <> w2)
+
+instance Monoid w => Monad (Writer w) where
+  wx >>= f =
+    let (x, w1) = runWriter wx
+        (r, w2) = runWriter (f x)
+    in Writer (r, w1 <> w2)
+
+fizzBuzz n = snd $ runWriter (go 1)
+  where
+    go i = do
+      if (i `mod` 15 == 0) then tell ["fizzbuzz"] else
+        if (i `mod` 5 == 0) then tell ["buzz"] else
+        if (i `mod` 3 == 0) then tell ["fizz"] else
+          tell [show i]
+      when (i < n) $
+        go $ i + 1
+
+fib = runWriter . go
+  where
+    go n = do
+      tell (Sum 1, [n])
+      case n of
+        1 -> pure 1
+        2 -> pure 1
+        _ -> do
+          a <- go (n - 1)
+          b <- go (n - 2)
+          pure (a + b)
+
+-- | State
+
+newtype State s a = State { runState :: s -> (a, s) }
+
+evalState :: State s a -> s -> a
+evalState x = fst . runState x
+
+execState :: State s a -> s -> s
+execState x = snd . runState x
+
+get :: State s s
+get = State $ \s -> (s, s)
+
+gets :: (s -> a) -> State s a
+gets f = f <$> get
+
+put :: s -> State s ()
+put x = State $ \_ -> ((), x)
+
+modify :: (s -> s) -> State s ()
+modify f = State $ \s -> ((), f s)
+-- modify = get >>= put . f
+
+instance Functor (State s) where
+  fmap f sx = State $ \s ->
+    let (x, s') = runState sx s
+    in (f x, s')
+
+instance Applicative (State s) where
+  pure x = State $ \s -> (x, s)
+
+  sf <*> sx = State $ \s ->
+    let
+      (f, s') = runState sf s
+      (x, s'') = runState sx s'
+    in (f x, s'')
+
+instance Monad (State s) where
+  sx >>= f = State $ \s ->
+    let (x, s') = runState sx s
+    in runState (f x) s'
+
+fibS n = evalState (go n) Map.empty
+  where
+    go 1 = pure 1
+    go 2 = pure 1
+    go i = do
+      cached <- gets (Map.lookup i)
+      case cached of
+        Just v -> pure v
+        _      -> do
+          a <- go (i - 1)
+          b <- go (i - 2)
+          let res = a + b
+          modify $ Map.insert i res
+          pure res
--- README.md
+++ README.md
@@ -3,4 +3,10 @@
 Lessons:
 
 1. [HS](Lesson01.hs)
-1. [LHS](Lesson02.lhs)+[HTML](https://astynax.github.io/tfpb2021/Lesson02.html)
+2. [LHS](Lesson02.lhs)+[HTML](https://astynax.github.io/tfpb2021/Lesson02.html)
+3. [HS](Lesson03.hs)
+4. [HS](Lesson04.hs)
+5. [HS](Lesson05.hs)
+6. [HS](Lesson06.hs)
+7. [HS](Lesson07.hs)
+8. [HS](Lesson08.hs)
